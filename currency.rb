fb0541e3a365ae1dcf5286dc094b330bac632580
Austin Bello <austin@automatedinsights.com>
2017-01-19 11:24:33 -0500

    not equal

--- currency.rb
+++ currency.rb
@@ -1,5 +1,6 @@
 class Currency
   attr_reader :amount, :code
+  attr_accessor :amount, :code
 
   def initialize(amount:, code:)
     @amount = amount
@@ -9,4 +10,16 @@ class Currency
   def ==(other)
     @amount == other.amount && @code == other.code
   end
+
+  def !=(other)
+    @amount != other.amount || @code != other.code
+  end
 end
+
+a = Currency.new(amount: 34, code: 'USD')
+b = Currency.new(amount: 34, code: 'USD')
+
+puts "Object A: #{a.amount} #{a.code}"
+puts "Object B: #{b.amount} #{b.code}"
+puts "Object A equals Object B: #{a == b}"
+puts "Object A does not equal to Object B: #{a != b}"
