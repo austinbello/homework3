17064c9e2bee207dc266a91e1ed17038b963f4bb
Austin Bello <austin@automatedinsights.com>
2017-01-19 11:39:51 -0500

    add

--- currency.rb
+++ currency.rb
@@ -14,12 +14,24 @@ class Currency
   def !=(other)
     @amount != other.amount || @code != other.code
   end
+
+  def + (other)
+    if @code == other.code
+      @amount += other.amount
+    else
+      puts "Currency objects must have the same currency code to be added"
+    end
+  end
 end
 
 a = Currency.new(amount: 34, code: 'USD')
 b = Currency.new(amount: 34, code: 'USD')
+c = Currency.new(amount: 27, code: 'EUR')
 
 puts "Object A: #{a.amount} #{a.code}"
 puts "Object B: #{b.amount} #{b.code}"
+puts "Object C: #{c.amount} #{c.code}"
 puts "Object A equals Object B: #{a == b}"
-puts "Object A does not equal to Object B: #{a != b}"
+puts "Object A does not equal to Object C: #{a != c}"
+a + b
+puts "Adding Object B to Object A = #{a.amount} #{a.code}"
