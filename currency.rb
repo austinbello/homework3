4cb979237abea668518e39e8558073811eedfe45
Austin Bello <austin@automatedinsights.com>
2017-01-19 13:29:36 -0500

    one argument

--- currency.rb
+++ currency.rb
@@ -2,9 +2,21 @@ class Currency
   attr_reader :amount, :code
   attr_accessor :amount, :code
 
-  def initialize(amount:, code:)
-    @amount = amount
-    @code = code
+  def initialize(options = {})
+    amount = options[:amount] || 0
+    code = options[:code] || ''
+
+    if amount.is_a? Numeric
+      @amount = amount
+      @code = code
+    else
+      num_split = amount.split /(?<=[^\d,\.])(?=[0-9])/
+      currency_codes = {"$" => "USD", "€" => "EUR", "£" => "GBP", "¥" => "JPY"}
+      if currency_codes.keys.any? { |code| num_split[0].include?(code) }
+        @amount = num_split[1]
+        @code = currency_codes[num_split[0]]
+      end
+    end
   end
 
   def ==(other)
@@ -38,7 +50,6 @@ class Currency
       puts "Must input a Fixnum or Float"
     end
   end
-
 end
 
 a = Currency.new(amount: 34, code: 'USD')
@@ -56,3 +67,5 @@ b - c
 puts "Subtracting Object C from Object B = #{b.amount} #{b.code}"
 c * 2
 puts "Multiplying Object C by 2 = #{c.amount} #{c.code}"
+d = Currency.new(amount: "$34.00")
+puts "Object D: #{d.amount} #{d.code}"
